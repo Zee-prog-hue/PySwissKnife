ff0974a43b417a203ceb7339bb721bb3234b2f2c
Zee-prog-hue <fulstazee@gmail.com>
2026-03-03 20:51:11 +0500

    Add files via upload

---
+++ PySwissKnife/qr_generator/gr_generator_p.py
@@ -0,0 +1,44 @@
+import qrcode
+from qrcode.exceptions import DataOverflowError
+
+def genrate_qr(data: str, file_path: str = "qr_code.png", box_size: int = 10, border: int = 5):
+    """
+    Generates a QR code image from the given data.
+
+    :param data: The text or URL to encode in the QR code.
+    :param filename: Output file name (PNG format recommended).
+    :param box_size: Size of each QR code box (pixels).
+    :param border: Border thickness (boxes).
+    """
+    try:
+        if not data.strip():
+            raise ValueError("Data for Qr Code cannot be empty.")
+        
+        # generate qr object
+        qr = qrcode.QRCode(
+            version=None, # automatic version
+            error_correction=qrcode.constants.ERROR_CORRECT_H, # High error correction
+            box_size=box_size,
+            border=border,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        # create qr image and save it
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(file_path)
+        print(f"QR code generated and saved to {file_path}")
+
+    except ValueError as ve:
+        print(f"Value Error: {ve}")
+    except TypeError as te:
+        print(f"Type Error: {te}")
+    except DataOverflowError:
+        print("Error: The provided data is too large to fit in a QR code.")
+    except Exception as e:
+        print(f"An error occurred: {e}") 
+
+
+if __name__ == "__main__":
+    user_data = input("Enter the data to encode in the QR code: ")
+    genrate_qr(user_data, file_path=input("Enter the file path to save the QR code (default: generated_qr_code.png): ") or "generated_qr_code.png")
